b36c94ce950d57a9bb53de5bc462e44ce5c15085
shahisrail <shahisrail134@gmail.com>
2025-03-05 21:59:24 +0600

    complted site

--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -16,7 +16,7 @@ function LoginForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:4000/api/auth/login", {
+    const res = await fetch("https://job-portal-backend-lake.vercel.app/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
